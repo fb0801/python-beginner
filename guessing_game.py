e2e769e42ca3defd207a9f6fda9ab424d93a2d5d
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-06 14:28:00 +0100

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -3,7 +3,7 @@ import random
 
 secret_word=[]
 hidden_word =["Follow", "Peter", "Mike", "Ashley", "Giraffe","Dragon"]
-guess=""
+guess=[]
 guess_count = 0
 guess_limit = 3
 out_of_guess = False
@@ -11,20 +11,21 @@ out_of_guess = False
 
 random_word = random.randint(0,5)
 chosen_word =hidden_word[random_word]
-    
-secret_word.append(chosen_word)
 
+secret_word.append(chosen_word)
+#print(secret_word)
 
     
 while guess != secret_word and not(out_of_guess):
     if guess_count < guess_limit:
         guess = input("Enter your guess human: ")
         guess_count +=1
+        #print(secret_word)
     else:
         out_of_guess = True
 
     
 if out_of_guess:
-    print("You lose human! your out of guesses")
+    print(f"You lose human! your out of guesses", "the word was {secret_word}")
 else:
     print("You win this time human!")
