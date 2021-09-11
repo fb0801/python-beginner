882d05e3ba207ea0c3e6961acc50bf2378b323a1
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-09-11 10:51:56 +0100

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,5 @@
+class Question:
+    def__init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
---
+++ app.py
@@ -0,0 +1,11 @@
+from Question import Question
+
+question_prompts = [
+    "what colour are apples?\n(a)Red\Green\n(b)Purple\n(c)Orange\n\n",
+    "what colour are Bananas?\n(a)Red\Green\n(b)Blue\n(c)Yellow\n\n",
+    "what colour are Strawberries?\n(a)Green\n(b)Red\n(c)Chocolate\n\n"]
+
+
+questions =[
+    Question(question_prompts[0],)
+    ]
